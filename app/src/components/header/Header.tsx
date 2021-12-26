695ceb1f8894730f726749d95b06f1c8f22b44c0
Arslanoov <rasularslanoov@gmail.com>
2021-12-26 15:24:51 +0500

    feat(icon): Added icons

--- app/src/components/header/Header.tsx
+++ app/src/components/header/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { HeaderWrapper } from './HeaderStyled';
+import { Wrapper, Nav, NavItem } from './HeaderStyled';
 
-const Header = () => <HeaderWrapper>Header</HeaderWrapper>;
+const Header = () => (
+  <Wrapper>
+    <Nav>
+      <NavItem>1</NavItem>
+      <NavItem>2</NavItem>
+      <NavItem>3</NavItem>
+      <NavItem>4</NavItem>
+    </Nav>
+  </Wrapper>
+);
 
 export default Header;
--- app/src/components/header/HeaderStyled.tsx
+++ app/src/components/header/HeaderStyled.tsx
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
-export const HeaderWrapper = styled.div`
-  background-color: orange;
+export const Wrapper = styled.div`
+  padding: 1rem 0;
+
+  background-color: #f60;
+`;
+
+export const Nav = styled.ul`
+  display: flex;
+  align-items: center;
+
+  list-style: none;
+`;
+
+export const NavItem = styled.li`
+  &:not(:last-of-type) {
+    margin-right: 1rem;
+  }
 `;
