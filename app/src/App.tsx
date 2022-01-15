bdda047ac613d6f2b6c8d62f2b010d7c0fb48da4
Arslanoov <rasularslanoov@gmail.com>
2022-01-15 10:53:20 +0500

    refactor(router): Splitted pages

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,21 +6,30 @@ import {
   Navigate,
 } from 'react-router-dom';
 
-import StoriesList from 'pages/stories';
-import Story from 'pages/story';
-import User from 'pages/user';
-
 import 'assets/styles/main.css';
+import MainLayout from './layouts/main/MainLayout';
+
+const StoriesList = React.lazy(() => import('pages/stories'));
+const Story = React.lazy(() => import('pages/story'));
+const User = React.lazy(() => import('pages/user'));
 
 const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Navigate to="/stories/top" />} />
-      <Route path="/stories/:type" element={<StoriesList />} />
-      <Route path="/story/:id" element={<Story />} />
-      <Route path="/user/:username" element={<User />} />
-    </Routes>
-  </Router>
+  <Suspense
+    fallback={
+      <Router>
+        <MainLayout />
+      </Router>
+    }
+  >
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/stories/top" />} />
+        <Route path="/stories/:type" element={<StoriesList />} />
+        <Route path="/story/:id" element={<Story />} />
+        <Route path="/user/:username" element={<User />} />
+      </Routes>
+    </Router>
+  </Suspense>
 );
 
 export default App;
